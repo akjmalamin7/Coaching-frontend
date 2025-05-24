824593587e530e3e8c80baa18c0d7314a1ab7976
akjmalamin7 <thpi205526@gmail.com>
2025-05-24 16:58:43 +0600

    updaste

---
+++ src/modules/batch/routes/create/BatchCreate.tsx
@@ -0,0 +1,11 @@
+import Container from "@/shared/ui/container/Container";
+import CreateBatchForm from "../../components/createBatchForm/CreateBatchForm";
+
+const BatchCreate = () => {
+  return (
+    <Container width="sm">
+      <CreateBatchForm />
+    </Container>
+  );
+};
+export default BatchCreate;
--- src/modules/batch/routes/index.tsx
+++ src/modules/batch/routes/index.tsx
@@ -1,18 +1,18 @@
 import { AppRoute } from "@/shared/routerType/router.type";
 import { lazy } from "react";
-const LazySubject = lazy(() => import("./create"));
-const LazySubjectList = lazy(() => import("./list"));
-export const subjectRoutes: AppRoute[] = [
+const LazyBatch = lazy(() => import("./create"));
+const LazyBatchList = lazy(() => import("./list"));
+export const batchRoutes: AppRoute[] = [
   {
     path: "/subject/create",
-    element: <LazySubject />,
+    element: <LazyBatch />,
     withLayout: true,
     isPrivate: true,
     role: ["admin"],
   },
   {
     path: "/subject/list",
-    element: <LazySubjectList />,
+    element: <LazyBatchList />,
     withLayout: true,
     isPrivate: true,
     role: ["admin"],
